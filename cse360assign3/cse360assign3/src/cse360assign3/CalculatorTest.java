5bda19dce26f9229481342bf5670332e6f424b34
Alex Aguayo <akaguayo@asu.edu>
2016-02-21 19:36:59 -0700

    CSE 360 Assignment 3 v1

---
+++ cse360assign3/cse360assign3/src/cse360assign3/CalculatorTest.java
@@ -0,0 +1,39 @@
+package cse360assign3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Test
+	public void testCalculator() {
+		Calculator calc = new Calculator();
+		assertNotNull(calc);
+	}
+	
+	public void testgetTotal(){
+		fail("Not yet implemented");
+	}
+	
+	public void testadd() {
+		fail("Not yet implemented");
+	}
+	
+	public void testsubtract() {
+		fail("Not yet implemented");
+	}
+	
+	public void testmultiply() {
+		fail("Not yet implemented");
+	}
+	
+	public void testdivide() {
+		fail("Not yet implemented");
+	}
+	
+	public void testgetHistory() {
+		fail("Not yet implemented");
+	}
+
+}
