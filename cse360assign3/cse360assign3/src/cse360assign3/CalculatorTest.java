6ec01e0cfa6462160b046fa3833a2937a8af0f3d
Alex Aguayo <akaguayo@asu.edu>
2016-02-21 20:24:23 -0700

    Second version created

--- cse360assign3/cse360assign3/src/cse360assign3/CalculatorTest.java
+++ cse360assign3/cse360assign3/src/cse360assign3/CalculatorTest.java
@@ -8,28 +8,60 @@ public class CalculatorTest {
 
 	@Test
 	public void testCalculator() {
+		//make sure calculator initializes
 		Calculator calc = new Calculator();
 		assertNotNull(calc);
 	}
 	
 	public void testgetTotal(){
-		fail("Not yet implemented");
+		Calculator calc = new Calculator();
+		assertEquals(calc.getTotal(), 0);
+
+		calc.add(3);
+		assertEquals(calc.getTotal(), 3);
 	}
 	
 	public void testadd() {
-		fail("Not yet implemented");
+		Calculator calc = new Calculator();
+		
+		calc.add(2);
+		assertEquals(calc.getTotal(), 2);
+		
+		calc.add(4);
+		assertEquals(calc.getTotal(), 6);
+		
+		calc.add(6);
+		assertEquals(calc.getTotal(), 12);
 	}
 	
 	public void testsubtract() {
-		fail("Not yet implemented");
+		Calculator calc = new Calculator();
+		
+		calc.subtract(3);
+		assertEquals(calc.getTotal(), -3);
 	}
 	
 	public void testmultiply() {
-		fail("Not yet implemented");
+		Calculator calc = new Calculator();
+		
+		calc.add(2);
+		calc.multiply(10);
+		assertEquals(calc.getTotal(), 20);
+		
+		calc.multiply(2);
+		assertEquals(calc.getTotal(), 40);
 	}
 	
 	public void testdivide() {
-		fail("Not yet implemented");
+		Calculator calc = new Calculator();
+		
+		calc.add(10);
+		calc.divide(2);
+		assertEquals(calc.getTotal(), 5);
+		
+		//test divide by 0 (should result in 0)
+		calc.divide(0);
+		assertEquals(calc.getTotal(), 0);
 	}
 	
 	public void testgetHistory() {
